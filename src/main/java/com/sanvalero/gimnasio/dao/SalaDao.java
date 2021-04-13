3a41f3dd3bec0d8fda153a4eb8f2a58e3e591254
santi_r6 <santi_r6@hotmail.es>
2021-04-13 10:56:53 +0100

    Clases DAO Socio, Tipo, Sala y borrar MovieDao

--- src/main/java/com/sanvalero/gimnasio/dao/SalaDao.java
+++ src/main/java/com/sanvalero/gimnasio/dao/SalaDao.java
@@ -1,5 +1,50 @@
 package com.sanvalero.gimnasio.dao;
 
+import com.sanvalero.gimnasio.domain.Sala;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class SalaDao {
-    
+
+    private Conexion conexion;
+    private int confirm;
+
+    public SalaDao(Conexion conexion) {
+        this.conexion = conexion;
+    }
+
+    public int crearSala(Sala sala) throws SQLException {
+        String sql = "INSERT INTO SALAS (NOMBRE_SALA, EXTENSION) VALUES (?, ?)";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, sala.getNombre());
+        sentencia.setString(2, String.valueOf(sala.getExtension()));
+        confirm = sentencia.executeUpdate();
+        return confirm;
+    }
+
+    public int borrarSala(Sala sala) throws SQLException {
+        String sql = "DELETE FROM SALAS WHERE ID_SALA = ?";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, sala.getIdSala());
+        confirm = sentencia.executeUpdate();
+        return confirm;
+    }
+
+    public ArrayList<Sala> listarSala(Sala sala) throws SQLException {
+        ArrayList<Sala> salas = new ArrayList<>();
+        String sql = "SELECT * FROM SALAS WHERE ID_SALA = ?";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, sala.getIdSala());
+        ResultSet rs = sentencia.executeQuery();
+        while (rs.next()) {
+            Sala salaAux = new Sala();
+            salaAux.setIdSala(rs.getString(1));
+            salaAux.setNombre(rs.getString(2));
+            salaAux.setExtension(rs.getInt(3));
+            salas.add(salaAux);
+        }
+        return salas;
+    }
 }
--- src/main/java/com/sanvalero/gimnasio/dao/SocioDao.java
+++ src/main/java/com/sanvalero/gimnasio/dao/SocioDao.java
@@ -1,5 +1,55 @@
 package com.sanvalero.gimnasio.dao;
 
+import com.sanvalero.gimnasio.domain.Sala;
+import com.sanvalero.gimnasio.domain.Socio;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class SocioDao {
-    
+
+    private Conexion conexion;
+    private int confirm;
+
+    public SocioDao(Conexion conexion) {
+        this.conexion = conexion;
+    }
+
+    public int crearSocio(Socio socio) throws SQLException {
+        String sql = "INSERT INTO SOCIOS (NOMBRE_SOCIO, APELLIDO_SOCIO, DNI, DIRECCION) VALUES ( ? , ? , ? , ? )";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, socio.getNombre());
+        sentencia.setString(2, socio.getApellido());
+        sentencia.setString(3, socio.getDni());
+        sentencia.setString(4, socio.getDireccion());
+        confirm = sentencia.executeUpdate();
+        return confirm;
+    }
+
+    public int borrarSocio(Socio socio) throws SQLException {
+        String sql = "DELETE FROM SOCIOS WHERE ID_SOCIO = ?";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, socio.getIdSocio());
+        confirm = sentencia.executeUpdate();
+        return confirm;
+    }
+
+    public ArrayList<Socio> listarSocio(Socio socio) throws SQLException {
+        ArrayList<Socio> socios = new ArrayList<>();
+        String sql = "SELECT * FROM SOCIOS WHERE ID_SOCIO = ?";
+        PreparedStatement sentencia = conexion.getConexion().prepareStatement(sql);
+        sentencia.setString(1, socio.getIdSocio());
+        ResultSet rs = sentencia.executeQuery();
+        while (rs.next()) {
+            Socio socioAux = new Socio();
+            socioAux.setIdSocio(rs.getString(1));
+            socioAux.setIdSocio(rs.getString(2));
+            socioAux.setIdSocio(rs.getString(3));
+            socioAux.setIdSocio(rs.getString(4));
+            socioAux.setIdSocio(rs.getString(5));
+            socios.add(socioAux);
+        }
+        return socios;
+    }
 }
